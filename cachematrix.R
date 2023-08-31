853e27dc2fe7af07c2036a053dd6be5cfdf8ffce
randonmess <lle_07@hotmail.com>
2023-08-30 17:24:10 -0700

    completed assignment; added in "caching inverse" message to let user know inverse has been cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix
 
-## Write a short comment describing this function
+## Returns list of functions for caching the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # initialize inverse as NULL
+        inv <- NULL
+        
+        # set value of matrix
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        # get value of matrix
+        get <- function() x
+        
+        # set value of inverse
+        setinv <- function(inverse) inv <<- inverse
+                
+        # get value of inverse
+        getinv <- function() inv
+        
+        # return list of functions
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x', checks cache first
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mtx <- x$get()
+        inv <- solve(mtx)
+        message("caching inverse")
+        x$setinv(inv)
+        inv
 }
